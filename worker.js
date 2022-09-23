c7aaf7a4a4df59f11bdc0f36dc3011a590b2c032
Nathan Clevenger <nateclev@gmail.com>
2022-09-23 14:28:25 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -18,17 +18,16 @@ export const api = {
 export default {
   fetch: async (req, env) => {
     const { user, origin, requestId, method, body, time, pathSegments, pathOptions, url, query } = await env.CTX.fetch(req).then(res => res.json())
-
     const options = {
       model: 'code-davinci-002',
-      prompt: '// ES6 arrow function called ',
+      prompt: '// ES6 arrow function called ' + pathSegments[0],
       temperature: 0,
       max_tokens: 300,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
     }
-
+    const completion = await fetch('https://api.openai.com/v1/completions', { method: 'post', body: JSON.stringify(options), headers:{ 'content-type': 'application/json', 'authorization': 'Bearer ' + env.OPENAI_API_KEY }}).then(res => res.json())
     return new Response(JSON.stringify({ api, requestId, url, pathSegments, pathOptions, options, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
   },
 }
