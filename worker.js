7761f7c81bbb92b1f5ea6d5e2800444388dcb484
Nathan Clevenger <nateclev@gmail.com>
2022-09-23 05:40:31 -0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,25 @@
+export const api = {
+  icon: '🤖',
+  name: 'gpt.do',
+  description: 'CGPT-3 Templates and Completions',
+  url: 'https://gpt.do/api',
+  type: 'https://apis.do/ai',
+  endpoints: {
+    list: 'https://gpt.do/list',
+    get: 'https://gpt.do/:id',
+  },
+  site: 'https://gpt.do',
+  login: 'https://gpt.do/login',
+  signup: 'https://gpt.do/signup',
+  subscribe: 'https://gpt.do/subscribe',
+  repo: 'https://github.com/drivly/gpt.do',
+}
+
+export default {
+  fetch: async (req, env) => {
+    const { user, origin, requestId, method, body, time, pathSegments, pathOptions, url, query } = await env.CTX.fetch(req).then(res => res.json())
+    
+
+    return new Response(JSON.stringify({ api, requestId, url, pathSegments, pathOptions, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
+  },
+}
