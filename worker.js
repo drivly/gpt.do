9cb9aa62ef7ef2cb35021c10a22aa7b78d6f0f41
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-03-30 12:14:41 -0500

    Added some additional content

--- worker.js
+++ worker.js
@@ -23,17 +23,15 @@ export default {
     }
     if (!user.authenticated) return Response.redirect('https://gpt.do/login')
     const options = {
-      model: 'code-davinci-002',
-      prompt: '// ES6 arrow function called ' + pathSegments[0],
-      temperature: 0,
-      max_tokens: 300,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
+      model: 'gpt-4',
+      messages: [
+        {'role': 'system', 'content': 'You are a helpful assistant.'},
+        {'role': 'user', 'content': pathSegments[0]},
+    ]
     }
-    const completion = await fetch('https://api.openai.com/v1/completions', { method: 'post', body: JSON.stringify(options), headers:{ 'content-type': 'application/json', 'authorization': 'Bearer ' + env.OPENAI_API_KEY }}).then(res => res.json())
-    const codeLines = completion.choices.map(choice => (options.prompt + choice.text).split('\n'))
-    return new Response(JSON.stringify({ api, options, completion, codeLines, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
+    const completion = await fetch('https://api.openai.com/v1/chat/completions', { method: 'post', body: JSON.stringify(options), headers:{ 'content-type': 'application/json', 'authorization': 'Bearer ' + env.OPENAI_API_KEY }}) //.then(res => res.json())
+    return completion
+    // return new Response(JSON.stringify({ api, options, completion, codeLines, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
   },
 }
 
