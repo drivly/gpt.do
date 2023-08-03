6493c2e6febea8b3ca586fcd4def3c6cb7552828
Samuel Lippert <lippertz@gmail.com>
2023-08-03 03:43:43 -0500

    Syntax

--- worker.js
+++ worker.js
@@ -95,7 +95,7 @@ export default {
       response: responses.length === 0 ? response : response.concat(responses.flatMap(s => s.flatMap(i => i))),
       ...(completions.length === 0 ? completion : {}),
       completions: completions.length === 0 ? undefined : [completion].concat(completions.flatMap(s => s.flatMap(i => i))),
-      inputMessages: query.debug && user.admin ? [messages].concat(inputMessages) : undefined,
+      inputMessages: query.debug && user.role === 'admin' ? [messages].concat(inputMessages) : undefined,
       user,
     })
   },
