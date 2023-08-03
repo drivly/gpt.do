86f797095333862e81d19debcfe9c651ad1899b5
Samuel Lippert <lippertz@gmail.com>
2023-08-03 02:59:09 -0500

    Record inputs for debug

--- worker.js
+++ worker.js
@@ -17,14 +17,13 @@ export const api = {
 
 export default {
   fetch: async (req, env) => {
-    const { user, json: data, pathname, pathSegments, query, }
-      = await env.CTX.fetch(req).then(res => res.json())
+    const { user, json: data, pathname, pathSegments, query, } = await env.CTX.fetch(req).then(res => res.json())
     if (pathname == '/favicon.ico') return new Response(null, { status: 404 })
-    if (pathname == '/webhooks/github') {
-      return json({ success: true, user })
-    }
+    if (pathname == '/webhooks/github') return json({ success: true, user })
     if (!user.authenticated) return Response.redirect('https://gpt.do/login')
     let { messages, functions, n, max_tokens, model, } = data || {}
+    if (!n) n = query.n
+    if (!max_tokens) max_tokens = query.max_tokens
     if (!model) model = query.model
     let forEach = [], input = { ...query }, file
     if (['prompts', 'formats'].includes(pathSegments[0])) {
@@ -37,29 +36,19 @@ export default {
         file = await fetch(decodeURIComponent(query.fileUrl)).then(res => res.text())
       }
       input = { ...template.input, ...query, file, }
-      if (Object.keys(input).length) {
-        messages = fillMessageTemplate(messages, input)
-      }
-      if (template.forEach?.length) {
-        forEach = Array.isArray(template.forEach[0]) ? template.forEach.map(formatMessages) : [formatMessages(template.forEach)]
-      }
+      if (Object.keys(input).length) messages = fillMessageTemplate(messages, input)
+      if (template.forEach?.length) forEach = Array.isArray(template.forEach[0]) ? template.forEach.map(formatMessages) : [formatMessages(template.forEach)]
     }
     if (!file && query.fileUrl) {
       file = await fetch(decodeURIComponent(query.fileUrl)).then(res => res.text())
       input.file = file
     }
-    if (file && !messages.find(m => m.role === 'user')) {
-      messages = [{ role: 'user', content: file }]
-    }
-    if (!messages?.length && pathSegments[0])
-      messages = [
-        { role: 'user', content: pathSegments[0].replace('_', ' ').replace('+', ' ') },
-      ]
+    if (file && !messages.find(m => m.role === 'user')) messages = [{ role: 'user', content: file }]
+    if (!messages?.length && pathSegments[0]) messages = [
+      { role: 'user', content: pathSegments[0].replace('_', ' ').replace('+', ' ') },
+    ]
     if (!messages) messages = []
-    if (!messages.find(m => m.role === 'system')) {
-      const content = query.system || data?.system || 'You are a helpful assistant who responds in Markdown.  All lists should be Markdown checklists with `- [ ]` items.'
-      messages.unshift({ role: 'system', content, })
-    }
+    if (!messages.find(m => m.role === 'system')) messages.unshift({ role: 'system', content: query.system || data?.system || 'You are a helpful assistant who responds in Markdown.  All lists should be Markdown checklists with `- [ ]` items.', })
     const options = {
       model: user.role === 'admin' && model ? model : 'gpt-3.5-turbo',
       messages,
@@ -76,28 +65,28 @@ export default {
     let response = completion.choices?.[0]?.message?.content?.split('\n')
     let completions = []
     let responses = []
-
+    let inputMessages = []
     for (let i = 0; i < forEach.length; i++) {
       forEach[i].items = i === 0 ? [response] : responses[i - 1]
       completions[i] = forEach[i].items.map(() => null)
       responses[i] = forEach[i].items.map(() => null)
+      inputMessages[i] = forEach[i].items.map(() => null)
       const promises = []
       for (let fork of forEach[i].items)
         for (let j = 0; j < fork.length; j++) {
+          inputMessages[i][j] = fillMessageTemplate(forEach[i], {
+            ...input,
+            item: fork[j].replace(/(^[\- \[\]"\\]*|"$)/g, '')
+          })
           promises.push(fetch('https://api.openai.com/v1/chat/completions', {
             method: 'post', body: JSON.stringify({
               ...options,
-              messages: fillMessageTemplate(forEach[i], {
-                ...input,
-                item: fork[j].replace(/(^[\- \[\]"\\]*|"$)/g, '')
-              })
+              messages: inputMessages[i][j]
             }), headers: { 'content-type': 'application/json', 'authorization': 'Bearer ' + env.OPENAI_API_KEY }
-          })
-            .then(res => res.json())
-            .then(c => {
-              completions[i][j] = c
-              responses[i][j] = c.error ? [] : c.choices?.[0]?.message?.content?.split('\n')
-            }))
+          }).then(res => res.json()).then(c => {
+            completions[i][j] = c
+            responses[i][j] = c.error ? [] : c.choices?.[0]?.message?.content?.split('\n')
+          }))
         }
       await Promise.all(promises)
     }
@@ -106,6 +95,7 @@ export default {
       response: responses.length === 0 ? response : response.concat(responses.flatMap(s => s.flatMap(i => i))),
       ...(completions.length === 0 ? completion : {}),
       completions: completions.length === 0 ? undefined : [completion].concat(completions.flatMap(s => s.flatMap(i => i))),
+      inputMessages: query.debug && inputMessages.length > 0 && user.admin ? [completion].concat(inputMessages.flatMap(s => s.flatMap(i => i))) : undefined,
       user,
     })
   },
