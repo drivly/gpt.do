f53d5496dd6a4b23ceffc5aac4489131fd08e9eb
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-04-18 02:33:51 -0500

    update auth config

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,6 @@ import { authConfig } from '@/app/(auth)/auth.config';
 
 export default NextAuth(authConfig).auth;
 
-// export const config = {
-//   matcher: [
-//     /*
-//      * Match all request paths except for the ones starting with:
-//      * - api/auth (auth API routes)
-//      */
-//     '/((?!api/auth).*)',
-//   ],
-// };
+export const config = {
+  matcher: ['/', '/:id', '/api/:path*', '/login', '/register'],
+};
